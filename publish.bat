929e4010d680568ae7cdf28a49cfa6e41194e4a3
Cameron Aavik <cameronaavik@gmail.com>
2016-12-31 15:56:50 +1000

    Moved the libraries to the root directory. Added a readme

--- publish.bat
+++ publish.bat
@@ -1,6 +1,6 @@
 dotnet publish -c Release -r win10-x64
 dotnet publish -c Release -r osx.10.10-x64
 dotnet publish -c Release -r ubuntu.14.04-x64
-copy ".\natives\glfw.dll" ".\bin\Release\netcoreapp1.0\win10-x64\publish\glfw.dll"
-copy ".\natives\libglfw.dylib" ".\bin\Release\netcoreapp1.0\win10-x64\publish\libglfw.dylib"
-copy ".\natives\libglfw.so" ".\bin\Release\netcoreapp1.0\win10-x64\publish\libglfw.so"
+copy ".\glfw.dll" ".\bin\Release\netcoreapp1.0\win10-x64\publish\glfw.dll"
+copy ".\libglfw.dylib" ".\bin\Release\netcoreapp1.0\win10-x64\publish\libglfw.dylib"
+copy ".\libglfw.so" ".\bin\Release\netcoreapp1.0\win10-x64\publish\libglfw.so"
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Cross-Platform Triangle in .NET Core
+
+Makes use of OpenGL and GLFW
