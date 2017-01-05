1318421d686eb59271c43f298362d1aff803c3cc
Cameron Aavik <cameronaavik@gmail.com>
2017-01-05 22:52:39 +1000

    Forgot to save my changes in the shell script

--- publish.sh
+++ publish.sh
@@ -1,12 +1,11 @@
-# Back up the source file
 mv Program.cs Program.cs.temp
-echo "#define WINDOWS" | cat . Program.cs.temp > Program.cs
+echo "#define WINDOWS" | cat - Program.cs.temp > Program.cs
 dotnet publish -c Release -r win10-x64
 rm Program.cs
-echo "#define OSX" | cat . Program.cs.temp > Program.cs
+echo "#define OSX" | cat - Program.cs.temp > Program.cs
 dotnet publish -c Release -r osx.10.10-x64
 rm Program.cs
-echo "#define LINUX" | cat . Program.cs.temp > Program.cs
+echo "#define LINUX" | cat - Program.cs.temp > Program.cs
 dotnet publish -c Release -r ubuntu.14.04-x64
 rm Program.cs
 mv Program.cs.temp Program.cs
